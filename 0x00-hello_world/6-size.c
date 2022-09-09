0d3ca4435280fc70568d3c9678e759331e307dfa
David Onyango <aqdave61@gmail.com>
2022-09-08 19:49:19 -0700

    Size not grandeur

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Return: Always 0 (success)
+**/
+int manin(void)
+{
+  int i;
+  char c;
+  long li;
+  long long lli;
+  float f;
+
+printf("Size of a int: %d\n",sizeof(i));
+printf("Size of a char: %d\n",sizeof(c));
+printf("Size of a long int: %d\n",sizeof(li));
+printf("Size of a long long int: %d\n",sizeof(lli));
+printf("Size of a float: %d\n",sizeof(f));
+return (0);
+}
